a070bc729eea65c8dfe62c2bbf1302c78f11d707
girdenis-p <girdenis.p@gmail.com>
2023-04-04 11:20:44 +0100

    add new task to production db

--- db/prod-seed.js
+++ db/prod-seed.js
@@ -8,5 +8,6 @@ VALUES
   ('prune', 'clean plants up to keep them happier'),
   ('look for bugs', 'not all bugs are bad though!'),
   ('health check', 'look out for signs of disease'),
-  ('harvest', 'enjoy your garden''s bounty')
+  ('harvest', 'enjoy your garden''s bounty'),
+  ('fertilise', 'your plants need food and nutrients')
 `);
